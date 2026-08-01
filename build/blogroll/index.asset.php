e95ef3e6d687b764ab53920286ebf04ee8816a24
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-08-01 20:34:01 +0200

    quieter meta row

--- build/blogroll/index.asset.php
+++ build/blogroll/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0d0b2641c8daae855764');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '172024a97240bd93a0da');
