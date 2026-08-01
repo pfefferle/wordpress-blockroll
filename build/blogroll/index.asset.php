8084c7b6bdbcae95670b9d2f8cc8cd24750601c9
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-08-01 21:00:19 +0200

    feed icon opens feed reader

--- build/blogroll/index.asset.php
+++ build/blogroll/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '92bfb24fd4140037c163');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c465a9064a308176a364');
--- src/blogroll/render.php
+++ src/blogroll/render.php
@@ -95,7 +95,10 @@ if ( 'manual' === ( $attributes['sortBy'] ?? 'name' ) ) {
 				<?php if ( $blockroll_link['feedUrl'] || $blockroll_link['xfn'] ) : ?>
 					<div class="blockroll-meta">
 						<?php if ( $blockroll_link['feedUrl'] ) : ?>
-							<a class="u-feed" rel="alternate noopener" target="_blank" type="application/rss+xml" href="<?php echo esc_url( $blockroll_link['feedUrl'] ); ?>"><svg class="blockroll-feed-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" focusable="false"><path d="M5 10.2h-.8v1.5H5c1.9 0 3.8.8 5.1 2.1 1.4 1.4 2.1 3.2 2.1 5.1v.8h1.5V19c0-2.3-.9-4.5-2.6-6.2-1.6-1.6-3.8-2.6-6.1-2.6zm10.4-1.6C12.6 5.8 8.9 4.2 5 4.2h-.8v1.5H5c3.5 0 6.9 1.4 9.4 3.9s3.9 5.8 3.9 9.4v.8h1.5V19c0-3.9-1.6-7.6-4.4-10.4zM4 20h3v-3H4v3z"></path></svg><?php esc_html_e( 'feed', 'blockroll' ); ?></a>
+							<span class="blockroll-feed">
+								<a href="<?php echo esc_url( 'feed:' . $blockroll_link['feedUrl'] ); ?>" aria-label="<?php esc_attr_e( 'Open in a feed reader', 'blockroll' ); ?>"><svg class="blockroll-feed-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" focusable="false"><path d="M5 10.2h-.8v1.5H5c1.9 0 3.8.8 5.1 2.1 1.4 1.4 2.1 3.2 2.1 5.1v.8h1.5V19c0-2.3-.9-4.5-2.6-6.2-1.6-1.6-3.8-2.6-6.1-2.6zm10.4-1.6C12.6 5.8 8.9 4.2 5 4.2h-.8v1.5H5c3.5 0 6.9 1.4 9.4 3.9s3.9 5.8 3.9 9.4v.8h1.5V19c0-3.9-1.6-7.6-4.4-10.4zM4 20h3v-3H4v3z"></path></svg></a>
+								<a class="u-feed" rel="alternate noopener" target="_blank" type="application/rss+xml" href="<?php echo esc_url( $blockroll_link['feedUrl'] ); ?>"><?php esc_html_e( 'feed', 'blockroll' ); ?></a>
+							</span>
 						<?php endif; ?>
 						<?php if ( $blockroll_link['feedUrl'] && $blockroll_link['xfn'] ) : ?>
 							<span class="blockroll-divider" aria-hidden="true">&#183;</span>
