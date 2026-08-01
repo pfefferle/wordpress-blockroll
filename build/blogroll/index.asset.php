a808f6814770e524d80eebe1abcd60120354fedd
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-08-01 21:51:55 +0200

    top align site icons

--- build/blogroll/index.asset.php
+++ build/blogroll/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c465a9064a308176a364');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8127e0973cc5110ecdb9');
