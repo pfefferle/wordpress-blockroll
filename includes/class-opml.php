c4c1bf6ddf416da2bb5d624ce0b5fe2a48d1d5f8
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-08-29 12:07:45 +0200

    add html discovery link

--- includes/class-opml.php
+++ includes/class-opml.php
@@ -296,15 +296,23 @@ class Opml {
 	}
 
 	/**
-	 * Print one rel="blogroll" link.
+	 * Print the rel="blogroll" links of one page: the OPML and the page
+	 * itself, so readers that want the HTML rather than the file find
+	 * it too.
 	 *
 	 * @param \WP_Post $post Post with a blogroll block.
 	 */
 	private static function print_discovery_link( $post ) {
+		$title = \esc_attr( self::title( $post ) );
 		\printf(
 			'<link rel="blogroll" type="text/xml" href="%s" title="%s" />' . "\n",
 			\esc_url( self::opml_url( $post ) ),
-			\esc_attr( self::title( $post ) )
+			$title
+		);
+		\printf(
+			'<link rel="blogroll" type="text/html" href="%s" title="%s" />' . "\n",
+			\esc_url( \get_permalink( $post ) ),
+			$title
 		);
 	}
 }
